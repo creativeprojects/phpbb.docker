abc74e174c1da6fa77299f7ec2473650e72aa82a
Fred <Fred@CreativeProjects.Tech>
2016-12-08 10:49:46 +0000

    Stop all containers

--- docker/stop.sh
+++ docker/stop.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+docker stop phpbb_creativeprojects_nginx
+docker stop phpbb_creativeprojects_php
 docker stop phpbb_creativeprojects_mysql
 
+docker rm -v phpbb_creativeprojects_nginx
+docker rm -v phpbb_creativeprojects_php
 docker rm -v phpbb_creativeprojects_mysql
